23f3d116ee82a11c019888883f919f1295195ed6
Niels Claes <niels.claes@live.be>
2020-02-12 15:58:57 +0100

    added a nice progress bar

--- python/utilities/tools.py
+++ python/utilities/tools.py
@@ -1,4 +1,18 @@
+import sys
+
 def custom_enumerate(iterable, start=0, step=1):
     for itr in iterable:
         yield start, itr
         start += step
+
+
+def progressbar(current, total, text=''):
+    bar_length = 40
+    progress = float(current) / float(total)
+    if progress >= 1.:
+        progress, status = 1, "\r\n"
+    block = int(round(bar_length * progress))
+    text = "\r[{}] {:.0f}% {}".format("#" * block + "-" * (bar_length - block), round(progress * 100, 0), text)
+    sys.stdout.write(text)
+    sys.stdout.flush()
+
