6d5a82fc5706acc4ab586d1c626dc0e821156bbf
Niels Claes <niels.claes@live.be>
2020-02-13 11:52:57 +0100

    fixed issues with paths

--- python/legolas_runner.py
+++ python/legolas_runner.py
@@ -1,12 +1,31 @@
 from utilities.parameters import DEFAULT_PARAMS
 from utilities.tools import custom_enumerate, progressbar
+from pathlib import Path
 
 import numpy as np
 import f90nml
 import subprocess
+import sys
 import os
 
+
+def _setup_and_check_paths():
+    main_file_dir = Path(__file__).parent
+    source_dir = (main_file_dir / '..').resolve()
+    multirun_dir = (source_dir / 'output/multiruns/').resolve()
+
+    if not Path(multirun_dir).is_dir():
+        print('Output directory not found, make sure Legolas is compiled!')
+        sys.exit(1)
+
+    return main_file_dir, source_dir, multirun_dir
+
+
 if __name__ == '__main__':
+
+    # Setup relative paths to output folders, check if output folders exist
+    file_dir, src_dir, multi_dir = _setup_and_check_paths()
+
     print('------------------------------------------')
     print('---- SCRIPT FOR ADVANCED LEGOLAS RUNS ----')
     print('------------------------------------------\n')
@@ -34,13 +53,10 @@ if __name__ == '__main__':
     gridpts = int(input("Gridpoints for each run?: "))
 
     param_list = np.linspace(lb, rb, nb_runs)
-
-    # generate parfiles and run legolas for each file. Do this from top directory.
-    os.chdir('..')
     namelist = {'gridlist': {}, 'equilibriumlist': {}, 'savelist': {}, 'filelist': {}}
     print('\nRunning Legolas...')
     for run, param in enumerate(param_list, start=1):
-        base_filename = 'multiruns/{:03d}_{}_{}_'.format(run, chosen_eq, varied_param)
+        base_filename = '{:03d}_{}_{}_'.format(run, chosen_eq, varied_param)
 
         namelist['gridlist']['gridpoints'] = gridpts
         namelist['equilibriumlist']['equilibrium_type'] = chosen_eq
@@ -56,9 +72,11 @@ if __name__ == '__main__':
         namelist['paramlist'].update({varied_param: param})
 
         # write the parfile, save to multiruns folder
-        parfile_name = 'output/' + base_filename + 'parfile.par'
+        parfile_name = (multi_dir / (base_filename + 'parfile.par')).resolve()
         f90nml.write(namelist, parfile_name, force=True)
 
         # run legolas
         progressbar(run, nb_runs, '{}/{}'.format(run, nb_runs))
+        os.chdir(str(src_dir))
         subprocess.check_call(['./legolas', '-i', parfile_name])
+        os.chdir(str(file_dir))
