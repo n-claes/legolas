43fc744d6d58bc07fe18f3263ea60618a8ce529f
Niels Claes <niels.claes@live.be>
2020-02-14 10:50:21 +0100

    paths cleanup for multiruns

--- python/legolas_runner.py
+++ python/legolas_runner.py
@@ -8,34 +8,35 @@ import subprocess
 import sys
 import os
 
+# define directories
+FILE_DIR = Path(__file__).parent
+SRC_DIR = (FILE_DIR / '..').resolve()
+MULTI_DIR = (SRC_DIR / 'output/multiruns/').resolve()
+if not Path(MULTI_DIR).is_dir():
+    print('Output directory not found, make sure Legolas is compiled!')
+    sys.exit(1)
 
-def _setup_and_check_paths():
-    main_file_dir = Path(__file__).parent
-    source_dir = (main_file_dir / '..').resolve()
-    multirun_dir = (source_dir / 'output/multiruns/').resolve()
+# make subdirectories
+Path.joinpath(MULTI_DIR, 'nml').mkdir(parents=True, exist_ok=True)
+Path.joinpath(MULTI_DIR, 'par').mkdir(parents=True, exist_ok=True)
+Path.joinpath(MULTI_DIR, 'dat').mkdir(parents=True, exist_ok=True)
 
-    if not Path(multirun_dir).is_dir():
-        print('Output directory not found, make sure Legolas is compiled!')
-        sys.exit(1)
-
-    return main_file_dir, source_dir, multirun_dir
+NML_DIR = MULTI_DIR.name + '/nml/'
+DAT_DIR = MULTI_DIR.name + '/dat/'
 
 
-if __name__ == '__main__':
-
-    # Setup relative paths to output folders, check if output folders exist
-    file_dir, src_dir, multi_dir = _setup_and_check_paths()
-
+def _main():
     print('------------------------------------------')
     print('---- SCRIPT FOR ADVANCED LEGOLAS RUNS ----')
     print('------------------------------------------\n')
 
     equils = iter(DEFAULT_PARAMS.keys())
     for idx, eq in custom_enumerate(equils, start=1, step=3):
-        print('{:03d} {:<30}{:03d} {:<30}{:03d} {}'.format(idx, eq, idx+1, next(equils, ""), idx+2, next(equils, "")))
+        print(
+            '{:03d} {:<30}{:03d} {:<30}{:03d} {}'.format(idx, eq, idx + 1, next(equils, ""), idx + 2, next(equils, "")))
 
     eq_nb = int(input("\nChoose equilibrium above: "))
-    chosen_eq = list(DEFAULT_PARAMS.keys())[eq_nb-1]
+    chosen_eq = list(DEFAULT_PARAMS.keys())[eq_nb - 1]
     print('Selected equilibrium: {}'.format(chosen_eq))
 
     print("\nAvailable parameters:")
@@ -63,8 +64,8 @@ if __name__ == '__main__':
         namelist['equilibriumlist']['use_defaults'] = False
         namelist['savelist']['run_silent'] = True
         namelist['savelist']['show_results'] = False
-        namelist['filelist']['savename_eigenvalues'] = multi_dir.name + '/' + base_filename + 'eigenvals'
-        namelist['filelist']['savename_config'] = multi_dir.name + '/' + base_filename + 'config'
+        namelist['filelist']['savename_eigenvalues'] = DAT_DIR + base_filename + 'eigenvals'
+        namelist['filelist']['savename_config'] = NML_DIR + base_filename + 'config'
 
         # get default parameters for this equilibrium, add to namelist
         namelist['paramlist'] = DEFAULT_PARAMS[chosen_eq]
@@ -73,11 +74,15 @@ if __name__ == '__main__':
         namelist['paramlist'].update({varied_param: param})
 
         # write the parfile, save to multiruns folder
-        parfile_name = (multi_dir / (base_filename + 'parfile.par')).resolve()
+        parfile_name = (Path.joinpath(MULTI_DIR, 'par') / (base_filename + 'parfile.par')).resolve()
         f90nml.write(namelist, parfile_name, force=True)
 
         # run legolas
         progressbar(run, nb_runs, '{}/{}'.format(run, nb_runs))
-        os.chdir(str(src_dir))
+        os.chdir(str(SRC_DIR))
         subprocess.check_call(['./legolas', '-i', parfile_name])
-        os.chdir(str(file_dir))
+        os.chdir(str(FILE_DIR))
+
+
+if __name__ == '__main__':
+    _main()
