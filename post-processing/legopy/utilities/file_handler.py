967397fd7a4971bff8773dbd9d2eff70677b248b
Niels Claes <niels.claes@live.be>
2020-04-16 12:07:51 +0200

    initial package commit

---
+++ post-processing/legopy/utilities/file_handler.py
@@ -0,0 +1,28 @@
+import struct
+import numpy as np
+
+ALIGN = '='
+
+def read_header(istream):
+    pass
+
+def read_grid(istream, header):
+    pass
+
+def read_grid_gauss(istream, header):
+    pass
+
+def read_ef_grid(istream, header):
+    pass
+
+def read_eigenvalues(istream, header):
+    pass
+
+def read_equilibrium_arrays(istream, header):
+    pass
+
+def read_eigenfunction(istream, header, ef_name):
+    pass
+
+
+
