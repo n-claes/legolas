071f67968c2038affa838195f3539b2cc3de5b59
Niels Claes <niels.claes@live.be>
2020-10-30 12:06:30 +0100

    added findSCALAPACK.cmake files, updates to BLAS/LAPACK

--- setup_tools/findBLAS.cmake
+++ setup_tools/findBLAS.cmake
@@ -1,7 +1,13 @@
-IF(BLAS_LIBRARIES)
-    SET(BLAS_FOUND TRUE)
-    RETURN()
+message(STATUS "Finding BLAS")
+
+IF($ENV{BLAS_LIBRARIES})
+    message(STATUS "BLAS found: $ENV{BLAS_LIBRARIES}")
+    set(BLAS_FOUND TRUE)
+    set(BLAS_LIBRARIES $ENV{BLAS_LIBRARIES})
+    return()
 ENDIF()
 
-message(STATUS "Finding BLAS")
 find_package(BLAS REQUIRED)
+if (NOT ${BLAS_FOUND})
+    message(STATUS "Try setting $BLAS_LIBRARIES")
+endif()
