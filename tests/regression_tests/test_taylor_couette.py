e2b2653a473c0dd1fc2436e783c69b9342ec7f7d
Niels Claes <niels.claes@live.be>
2022-07-01 15:44:28 +0200

    shift-invert regression tests for taylor-couette flow

--- tests/regression_tests/test_taylor_couette.py
+++ tests/regression_tests/test_taylor_couette.py
@@ -2,9 +2,7 @@ from .regression import RegressionTest
 import pytest
 
 
-class TestTaylorCouetteQR(RegressionTest):
-    name = "Taylor Couette k2=0 k3=1"
-    filename = "taylor_couette_QR_k2_0_k3_1"
+class TaylorCouette(RegressionTest):
     equilibrium = "taylor_couette"
     geometry = "cylindrical"
     x_start = 1
@@ -17,7 +15,6 @@ class TestTaylorCouetteQR(RegressionTest):
         "alpha": 1.0,
         "beta": 2.0,
     }
-
     physics_settings = {
         "flow": True,
         "coaxial": True,
@@ -31,6 +28,10 @@ class TestTaylorCouetteQR(RegressionTest):
         "eigenfunction_subset_radius": 0.5,
     }
 
+
+class TestTaylorCouetteQR(TaylorCouette):
+    name = "Taylor Couette k2=0 k3=1 QR"
+    filename = "taylor_couette_QR_k2_0_k3_1"
     spectrum_limits = [
         {"xlim": (-1100, 1100), "ylim": (-155, 5)},
         {"xlim": (-175, 175), "ylim": (-35, 4)},
@@ -43,6 +44,36 @@ class TestTaylorCouetteQR(RegressionTest):
         {"eigenvalue": 0.34390 - 0.13832j},
         {"eigenvalue": 0.26543 - 0.21655j},
         {"eigenvalue": 0.21295 - 0.31135j},
+    ]
+
+    @pytest.mark.parametrize("limits", spectrum_limits)
+    def test_spectrum(self, limits, ds_test, ds_base):
+        super().run_spectrum_test(limits, ds_test, ds_base)
+
+    @pytest.mark.parametrize("eigenfunction", eigenfunctions)
+    def test_eigenfunction(self, eigenfunction, ds_test, ds_base):
+        super().run_eigenfunction_test(eigenfunction, ds_test, ds_base)
+
+
+class TestTaylorCouetteSI(TaylorCouette):
+    name = "Taylor Couette k2=0 k3=1 shift-invert"
+    filename = "taylor_couette_SI_k2_0_k3_1"
+    solver_settings = {
+        "solver": "arnoldi",
+        "arpack_mode": "shift-invert",
+        "number_of_eigenvalues": 4,
+        "which_eigenvalues": "LM",
+        "sigma": 0.2 - 0.2j,
+    }
+
+    spectrum_limits = [
+        {"xlim": (-0.01, 0.35), "ylim": (-0.45, 0.01)},
+    ]
+    eigenfunctions = [
+        {"eigenvalue": 0.34390 - 0.13832j},
+        {"eigenvalue": 0.26543 - 0.21655j},
+        {"eigenvalue": 0.21295 - 0.31135j},
+        {"eigenvalue": 0.16851 - 0.42393j},
     ]
 
     @pytest.mark.parametrize("limits", spectrum_limits)
