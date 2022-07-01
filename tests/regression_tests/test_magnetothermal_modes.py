ca01bc2991cd703bee6b80b8f2ed3d09c472d2c6
Niels Claes <niels.claes@live.be>
2022-07-01 14:34:30 +0200

    shift-invert regression tests for magnetothermal modes

--- tests/regression_tests/test_magnetothermal_modes.py
+++ tests/regression_tests/test_magnetothermal_modes.py
@@ -3,9 +3,7 @@ import pytest
 import numpy as np
 
 
-class TestMagnetoThermalModesQR(RegressionTest):
-    name = "magnetothermal modes k2=0 k3=1 QR"
-    filename = "magnetothermal_QR_k2_0_k3_1"
+class MagnetoThermalModes(RegressionTest):
     equilibrium = "magnetothermal_instabilities"
     geometry = "cylindrical"
 
@@ -22,6 +20,10 @@ class TestMagnetoThermalModesQR(RegressionTest):
         "mean_molecular_weight": 1.0,
     }
 
+
+class TestMagnetoThermalModesQR(MagnetoThermalModes):
+    name = "magnetothermal modes k2=0 k3=1 QR"
+    filename = "magnetothermal_QR_k2_0_k3_1"
     spectrum_limits = [
         {"xlim": (-650, 650), "ylim": (-0.15, 0.12)},
         {"xlim": (-40, 40), "ylim": (-0.15, 0.12)},
@@ -46,3 +48,35 @@ class TestMagnetoThermalModesQR(RegressionTest):
             assert (
                 ds_test.units.get(f"unit_{val}") == self.physics_settings[f"unit_{val}"]
             )
+
+
+class TestMagnetoThermalModesSI(MagnetoThermalModes):
+    name = "magnetothermal modes k2=0 k3=1 shift-invert"
+    filename = "magnetothermal_SI_k2_0_k3_1"
+    solver_settings = {
+        "solver": "arnoldi",
+        "arpack_mode": "shift-invert",
+        "number_of_eigenvalues": 15,
+        "which_eigenvalues": "LM",
+        "sigma": 0.01 + 0.04j,
+    }
+
+    spectrum_limits = [
+        {"xlim": (-0.001, 0.022), "ylim": (-0.001, 0.05)},
+    ]
+    eigenfunction_settings = {"write_eigenfunctions": True}
+    eigenfunctions = [
+        {"eigenvalue": 2.02095e-02 + 0.04415j},
+        {"eigenvalue": 2.02365e-02 + 0.03219j},
+        {"eigenvalue": 1.89167e-02 + 0.02587j},
+        {"eigenvalue": 2.12871e-13 + 0.02799j},
+        {"eigenvalue": 1.74600e-02 + 0.02195j},
+    ]
+
+    @pytest.mark.parametrize("limits", spectrum_limits)
+    def test_spectrum(self, limits, ds_test, ds_base):
+        super().run_spectrum_test(limits, ds_test, ds_base)
+
+    @pytest.mark.parametrize("eigenfunction", eigenfunctions)
+    def test_eigenfunction(self, eigenfunction, ds_test, ds_base):
+        super().run_eigenfunction_test(eigenfunction, ds_test, ds_base)
