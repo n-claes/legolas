3858aabe199d7de62f0bd6aa51b69125071b70be
nicolasbrughmans <nicolasbrughmans333@gmail.com>
2023-04-11 16:22:30 +0200

    Using annotations instead of quotation marks

--- post_processing/pylbo/utilities/datfiles/file_reader.py
+++ post_processing/pylbo/utilities/datfiles/file_reader.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from os import PathLike
 from typing import BinaryIO
 
@@ -121,7 +122,7 @@ class LegolasFileReader:
 
     def read_matrix_A(
         self, header: LegolasHeader
-    ) -> "tuple[np.ndarray, np.ndarray, np.ndarray]":
+    ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
         with open(self.datfile, "rb") as istream:
             hdr = read_mixed_from_istream(
                 istream,
@@ -139,7 +140,7 @@ class LegolasFileReader:
 
     def read_matrix_B(
         self, header: LegolasHeader
-    ) -> "tuple[np.ndarray, np.ndarray, np.ndarray]":
+    ) -> tuple[np.ndarray, np.ndarray, np.ndarray]:
         with open(self.datfile, "rb") as istream:
             hdr = read_mixed_from_istream(
                 istream,
