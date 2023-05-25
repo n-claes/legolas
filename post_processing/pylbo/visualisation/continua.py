d29e4d33896614748bfd8b2f006b29bb492e460e
Niels Claes <niels.claes@live.be>
2023-05-25 11:28:53 +0200

    pylbo: refactor continua + include L0 in thermal

--- post_processing/pylbo/visualisation/continua.py
+++ post_processing/pylbo/visualisation/continua.py
@@ -1,3 +1,10 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Tuple
+
+if TYPE_CHECKING:
+    from pylbo.data_containers import LegolasDataSet
+
 import numpy as np
 from pylbo.utilities.logger import pylboLogger
 from pylbo.visualisation.legend_handler import LegendHandler
@@ -19,14 +26,50 @@ CONTINUA_NAMES = {
 }
 CONTINUA_COLORS = ["red", "red", "cyan", "cyan", "green", "grey"]
 
-
-def is_zero(values):
-    return np.all(np.isclose(values, 0, atol=1e-10))
+_DEFAULT_ZERO_TOL = 1e-12
 
 
-def calculate_continua(ds):
+def _is_zero(values: np.ndarray, zero_tol: float = _DEFAULT_ZERO_TOL) -> bool:
+    return np.all(np.isclose(values, 0, atol=zero_tol))
+
+
+def _is_nonadiabatic(ds: LegolasDataSet) -> bool:
+    zeroes = np.zeros_like(ds.grid_gauss)
+    dLdT = ds.equilibria.get("dLdT", zeroes)
+    dLdrho = ds.equilibria.get("dLdrho", zeroes)
+    has_cooling = not _is_zero(dLdT) or not _is_zero(dLdrho)
+    has_conduction = not _is_zero(ds.equilibria.get("kappa_para", zeroes))
+    return has_cooling or has_conduction
+
+
+def _get_parallel_wave_vector(ds: LegolasDataSet) -> float:
+    bg = ds.equilibria
+    k2 = ds.parameters.get("k2", 0)
+    k3 = ds.parameters.get("k3", 0)
+    B0 = bg["B0"]
+    eps = ds.scale_factor
+    return 0 if _is_zero(B0) else (k2 * bg["B02"] / eps + k3 * bg["B03"]) / B0
+
+
+def _get_squared_sound_speed(ds: LegolasDataSet) -> np.ndarray:
+    return ds.gamma * ds.equilibria["T0"]
+
+
+def _get_squared_isothermal_sound_speed(ds: LegolasDataSet) -> np.ndarray:
+    return ds.equilibria["T0"]
+
+
+def _get_squared_Alfven_speed(ds: LegolasDataSet) -> np.ndarray:
+    return ds.equilibria["B0"] ** 2 / ds.equilibria["rho0"]
+
+
+def calculate_continua(ds: LegolasDataSet) -> dict:
     """
     Calculates the different continua for a given dataset.
+    The Alfvén and flow continua are always analytical. Depending on the background
+    and physical effects the slow and thermal continua are either all analytical, or
+    coupled through a third-order polynomial.
+    In case of the latter this polynomical is numerically solved through numpy.roots.
 
     Parameters
     ----------
@@ -35,127 +78,21 @@ def calculate_continua(ds):
 
     Returns
     -------
-    continua : dict, None
-        Dictonary containing the various continua as numpy arrays. If the
-        dataset does not contain a background, `None` is returned.
+    dict, None
+        Dictionary containing the various continua. The keys are the names of the
+        continua, the values are the corresponding frequencies as numpy arrays.
+        Returns `None` if the dataset has no background.
     """
     if not ds.has_background:
         return None
 
-    rho = ds.equilibria["rho0"]
-    B02 = ds.equilibria["B02"]
-    B03 = ds.equilibria["B03"]
-    B0 = ds.equilibria["B0"]
-    v02 = ds.equilibria["v02"]
-    v03 = ds.equilibria["v03"]
-    T = ds.equilibria["T0"]
-    p = rho * T
-
-    k2 = ds.parameters["k2"]
-    k3 = ds.parameters["k3"]
-    gamma = ds.gamma
-
-    # Alfven and slow continuum (squared)
-    alfven2 = (1 / rho) * ((k2 * B02 / ds.scale_factor) + k3 * B03) ** 2
-    slow2 = (gamma * p / (gamma * p + B0**2)) * alfven2
-    # doppler shift equals dot product of k and v
-    doppler = k2 * v02 / ds.scale_factor + k3 * v03
-    slow_min = -np.sqrt(slow2)
-    slow_plus = np.sqrt(slow2)
-
-    # Thermal continuum calculation
-    # wave vector parallel to magnetic field, uses vector projection and scale factor
-    if not all(np.isclose(B0, 0, atol=1e-10)):
-        # take care of hydro cases, this will throw a division by 0 warning
-        kpara = (k2 * B02 / ds.scale_factor + k3 * B03) / B0
-    else:
-        kpara = 0
-    cs2 = gamma * p / rho  # sound speed
-    vA2 = B0**2 / rho  # Alfvén speed
-    ci2 = p / rho  # isothermal sound speed
-    dLdT = ds.equilibria["dLdT"]
-    dLdrho = ds.equilibria["dLdrho"]
-    kappa_para = ds.equilibria["kappa_para"]
-    kappa_perp = ds.equilibria["kappa_perp"]
-    # if there is no conduction/cooling, there is no thermal continuum.
-    if (
-        is_zero(dLdT)
-        and is_zero(dLdrho)
-        and is_zero(kappa_para)
-        and is_zero(kappa_perp)
-    ):
-        thermal = np.zeros_like(ds.grid_gauss)
-    # if temperature is zero (no pressure), set to zero and return
-    elif is_zero(T):
-        thermal = np.zeros_like(ds.grid_gauss)
-    # if slow continuum vanishes, thermal continuum is analytical
-    elif is_zero(slow2):
-        thermal = 1j * (gamma - 1) * (rho * dLdrho - dLdT * (ci2 + vA2)) / (cs2 + vA2)
-    else:
-        sigma_A2 = kpara**2 * vA2  # Alfvén frequency
-        sigma_c2 = cs2 * sigma_A2 / (vA2 + cs2)  # cusp frequency
-        sigma_i2 = ci2 * sigma_A2 / (vA2 + ci2)  # isothermal cusp frequency
-
-        # thermal and slow continuum are coupled in a third degree polynomial in omega,
-        # coeffi means the coefficient corresponding to the term omega^i
-        coeff3 = rho * (cs2 + vA2) * 1j / (gamma - 1)
-        coeff2 = (
-            -(kappa_para * kpara**2 + rho * dLdT) * (ci2 + vA2) + rho**2 * dLdrho
-        )
-        coeff1 = -rho * (cs2 + vA2) * sigma_c2 * 1j / (gamma - 1)
-        coeff0 = (kappa_para * kpara**2 + rho * dLdT) * (
-            ci2 + vA2
-        ) * sigma_i2 - rho**2 * dLdrho * sigma_A2
-        # we have to solve this equation "gauss_gridpts" times.
-        # the thermal continuum corresponds to the (only) purely imaginary solution,
-        # modified slow continuum are other two solutions
-        thermal = np.empty(shape=len(ds.grid_gauss), dtype=complex)
-        slow_min = np.empty_like(thermal)
-        slow_plus = np.empty_like(thermal)
-        warning_logged = False
-        for idx, _ in enumerate(ds.grid_gauss):
-            solutions = np.roots([coeff3[idx], coeff2[idx], coeff1[idx], coeff0[idx]])
-            # create mask for purely imaginary solutions
-            mask = np.isclose(abs(solutions.real), 0)
-            # this should only have 1 True element, the thermal solution
-            if np.count_nonzero(mask) == 1:
-                s_neg, s_pos = np.sort_complex(solutions[np.invert(mask)])
-            else:
-                if not warning_logged:
-                    pylboLogger.warning(
-                        f"encountered at least 1 point where the slow continuum has a "
-                        f"real value close to zero. \nContinuum solutions: {solutions}"
-                    )
-                    warning_logged = True
-                mask = np.array([False] * 3, dtype=bool)
-                # check if this already happens on the first index
-                if idx == 0:
-                    # if so, assume largest mode is the thermal one and complex sort
-                    # slow+ and slow- solutions
-                    mask[solutions.imag.argmax()] = True
-                    pylboLogger.warning(
-                        f"this happened already at the first gridpoint, so we assumed "
-                        f"{solutions[mask]} to be the thermal mode"
-                    )
-                    s_neg, s_pos = np.sort_complex(solutions[np.invert(mask)])
-                else:
-                    # if not, get solution closest to previous thermal mode
-                    mask[np.abs(solutions - thermal[idx - 1]).argmin()] = True
-                    # retrieve closest slow- solution
-                    slow_solutions = solutions[np.invert(mask)]
-                    idx_neg = np.abs(slow_solutions - slow_min[idx - 1]).argmin()
-                    s_neg = slow_solutions[idx_neg]
-                    # remaining value is slow+ solution, remove slow- from array
-                    (s_pos,) = np.delete(slow_solutions, idx_neg)
-            # extract continuum solutions
-            (thermal[idx],) = solutions[mask]
-            slow_min[idx] = s_neg
-            slow_plus[idx] = s_pos
-
+    doppler = get_doppler_shift(ds)
+    alfven2 = get_squared_alfven_continuum(ds)
+    slowneg, slowpos, thermal = _get_thermal_and_slow_continua(ds)
     continua = {
         DOPPLER: doppler,
-        SLOW_MIN: slow_min,  # minus already accounted for
-        SLOW_PLUS: slow_plus,
+        SLOW_MIN: slowneg,
+        SLOW_PLUS: slowpos,
         THERMAL: thermal,
         ALFVEN_MIN: -np.sqrt(alfven2),
         ALFVEN_PLUS: np.sqrt(alfven2),
@@ -164,9 +101,202 @@ def calculate_continua(ds):
     for name in CONTINUA_NAMES.keys():
         if name == DOPPLER:
             continue
-        if not is_zero(continua[name]):
+        if not _is_zero(continua[name]):
             continua[name] += doppler
     return continua
+
+
+def get_squared_alfven_continuum(ds: LegolasDataSet) -> np.ndarray:
+    """
+    Calculates the squared Alfvén continuum.
+
+    Returns
+    -------
+    np.ndarray
+        The squared Alfvén continuum.
+    """
+    bg = ds.equilibria
+    k2 = ds.parameters.get("k2", 0)
+    k3 = ds.parameters.get("k3", 0)
+    eps = ds.scale_factor
+    return (1 / bg["rho0"]) * (k2 * bg["B02"] / eps + k3 * bg["B03"]) ** 2
+
+
+def get_doppler_shift(ds: LegolasDataSet) -> np.ndarray:
+    """
+    Calculates the Doppler shift as the dot product between the wave vector and the
+    background velocity.
+
+    Returns
+    -------
+    np.ndarray
+        The Doppler shift.
+    """
+    bg = ds.equilibria
+    k2 = ds.parameters.get("k2", 0)
+    k3 = ds.parameters.get("k3", 0)
+    eps = ds.scale_factor
+    return k2 * bg["v02"] / eps + k3 * bg["v03"]
+
+
+def get_squared_slow_continuum(ds: LegolasDataSet) -> np.ndarray:
+    """
+    Calculates the squared slow continuum.
+
+    Returns
+    -------
+    np.ndarray
+        The squared slow continuum.
+    """
+    bg = ds.equilibria
+    p = bg["rho0"] * bg["T0"]
+    alfven_sq = get_squared_alfven_continuum(ds)
+    return (ds.gamma * p / (ds.gamma * p + bg["B0"] ** 2)) * alfven_sq
+
+
+def _get_thermal_and_slow_continua(
+    ds: LegolasDataSet,
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Calculates the thermal and slow continua.
+
+    Returns
+    -------
+    Tuple[np.ndarray, np.ndarray, np.ndarray]
+        A tuple containing the slow-, slow+ and thermal continua, in this order.
+    """
+    zeroes = np.zeros_like(ds.grid_gauss)
+
+    bg = ds.equilibria
+    T0 = bg["T0"]
+    if _is_zero(T0):
+        # for pressureless cases (no T) there is no slow/thermal continuum
+        return (zeroes, zeroes, zeroes)
+    slow_sq = get_squared_slow_continuum(ds)
+    if not _is_nonadiabatic(ds):
+        return (-np.sqrt(slow_sq), np.sqrt(slow_sq), zeroes)
+    if _is_zero(slow_sq):
+        # if slow continuum vanishes, thermal continuum is analytical
+        thermal = _get_thermal_continuum_analytical(ds)
+        return (zeroes, zeroes, thermal)
+    # for standard cases we have a third-order polynomial
+    return _get_slow_and_thermal_continuum_coupled(ds)
+
+
+def _get_thermal_continuum_analytical(ds: LegolasDataSet) -> np.ndarray:
+    """
+    Calculates the thermal continuum analytically, when the slow continuum is zero.
+
+    Returns
+    -------
+    np.ndarray
+        The thermal continuum.
+    """
+    zeroes = np.zeros_like(ds.grid_gauss)
+    bg = ds.equilibria
+    L0 = bg.get("L0", zeroes)
+    dLdT = bg.get("dLdT", zeroes)
+    dLdrho = bg.get("dLdrho", zeroes)
+    rho0 = bg["rho0"]
+    cs2 = _get_squared_sound_speed(ds)
+    ca2 = _get_squared_Alfven_speed(ds)
+    ci2 = _get_squared_isothermal_sound_speed(ds)
+    gamma_1 = ds.gamma - 1
+    return 1j * gamma_1 * (L0 + rho0 * dLdrho - (ca2 + ci2) * dLdT) / (cs2 + ca2)
+
+
+def _get_slow_and_thermal_continuum_coupled(
+    ds: LegolasDataSet,
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    cs2 = _get_squared_sound_speed(ds)
+    ca2 = _get_squared_Alfven_speed(ds)
+    ci2 = _get_squared_isothermal_sound_speed(ds)
+    kpara = _get_parallel_wave_vector(ds)
+    gamma_1 = ds.gamma - 1
+    zeroes = np.zeros_like(ds.grid_gauss)
+    L0 = ds.equilibria.get("L0", zeroes)
+    dLdT = ds.equilibria.get("dLdT", zeroes)
+    dLdrho = ds.equilibria.get("dLdrho", zeroes)
+    kappa_para = ds.equilibria.get("kappa_para", zeroes)
+    rho0 = ds.equilibria["rho0"]
+
+    # coeffi means the coefficient corresponding to the term omega^i
+    coeff3 = rho0 * (cs2 + ca2) * 1j / gamma_1
+    coeff2 = rho0 * (L0 + rho0 * dLdrho) - (kappa_para * kpara**2 + rho0 * dLdT) * (
+        ci2 + ca2
+    )
+    coeff1 = -rho0 * cs2 * ca2 * kpara**2 * 1j / gamma_1
+    coeff0 = -(
+        (rho0 * (L0 + rho0 * dLdrho) - (kappa_para * kpara**2 + rho0 * dLdT) * ci2)
+        * ca2
+        * kpara**2
+    )
+    return _solve_coupled_continuum_polynomial(coeff3, coeff2, coeff1, coeff0)
+
+
+def _solve_coupled_continuum_polynomial(
+    coeff3: np.ndarray, coeff2: np.ndarray, coeff1: np.ndarray, coeff0: np.ndarray
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Solves the third-order polynomial that couples the slow and thermal continua.
+    The thermal continuum corresponds to the purely imaginary solution.
+
+    Parameters
+    ----------
+    coeff3 : np.ndarray
+        The coefficient corresponding to the term :math:`\\omega^3`.
+    coeff2 : np.ndarray
+        The coefficient corresponding to the term :math:`\\omega^2`.
+    coeff1 : np.ndarray
+        The coefficient corresponding to the term :math:`\\omega^1`.
+    coeff0 : np.ndarray
+        The coefficient corresponding to the constant term.
+
+    Returns
+    -------
+    Tuple[np.ndarray, np.ndarray, np.ndarray]
+        A tuple containing the slow-, slow+ and thermal continua, in this order.
+    """
+    thermal = np.zeros_like(coeff3, dtype=complex)
+    slowneg = np.zeros_like(thermal, dtype=complex)
+    slowpos = np.zeros_like(thermal, dtype=complex)
+    for i, (c3, c2, c1, c0) in enumerate(zip(coeff3, coeff2, coeff1, coeff0)):
+        roots = np.roots([c3, c2, c1, c0])
+        slowneg[i], slowpos[i], thermal[i] = _extract_solutions_from_roots(roots, i)
+    return slowneg, slowpos, thermal
+
+
+def _extract_solutions_from_roots(
+    roots: np.ndarray, i: int = 0
+) -> Tuple(complex, complex, complex):
+    # create mask for purely imaginary roots
+    mask = np.isclose(abs(roots.real), 0, atol=_DEFAULT_ZERO_TOL)
+    if np.count_nonzero(mask) == 1:
+        # if mask only has one True, it is the thermal continuum and others are slow
+        ws_neg, ws_pos = np.sort_complex(roots[np.invert(mask)])
+        return ws_neg, ws_pos, roots[mask]
+    _log_slow_continuum_zero_warning(roots, i)
+    # here we have multiple purely imaginary roots, so it's not clear which one
+    # is the thermal continuum. We assume that it is the largest one.
+    mask = np.array(["False"] * 3, dtype=bool)
+    mask[roots.imag.argmax()] = True
+    thermal = roots[mask]
+    _log_assumed_thermal_continuum(root=thermal)
+    ws_neg, ws_pos = np.sort_complex(roots[np.invert(mask)])
+    return ws_neg, ws_pos, thermal
+
+
+def _log_slow_continuum_zero_warning(roots: np.ndarray, i: int):
+    pylboLogger.warning(
+        f"encountered index = {i} where the slow continuum has a "
+        f"real value close to zero. \nFound thermal-slow roots: {roots}"
+    )
+
+
+def _log_assumed_thermal_continuum(root: complex):
+    pylboLogger.warning(
+        f"Assuming that the largest imaginary root {root} is the thermal continuum."
+    )
 
 
 class ContinuaHandler(LegendHandler):
