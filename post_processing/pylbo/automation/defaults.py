a49c5f7070ddff824e10f7e741d4817ef144f4f5
nicolasbrughmans <nicolasbrughmans333@gmail.com>
2026-07-06 11:54:47 +0200

    Typo

--- post_processing/pylbo/automation/defaults.py
+++ post_processing/pylbo/automation/defaults.py
@@ -311,6 +311,7 @@ amrvac_namelist_items = {
         ("gamma1_method", str),
         ("p2eint_method", str),
         ("ionE", bool),
-        ("He_abundance", (int, np.integer, float))("gamma", (int, np.integer, float)),
+        ("He_abundance", (int, np.integer, float)),
+        ("gamma", (int, np.integer, float)),
     ],
 }
